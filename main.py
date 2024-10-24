bff8f501880de64b212665e53d21064025652e4f
Jasurbek <jasurbekx98@gamil.com>
2024-10-24 14:38:34 +0500

    28-misol

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-# leetcode 58 - misol
+# # leetcode 58 - misol
 s = 'Hello world'
 a = s.split()
 b = len(a[-1])
 print(b)
+
+# 28 - misol
+def strStr(haystack: str, needle: str) -> int:
+    l = len(haystack)
+    l1 = len(needle)
+    for i in range(l):
+        if haystack[i:i+l1] == needle:
+            return i
+    return -1
+
+haystack = 'sadbutsad'
+needle = ''
+print(strStr(haystack, needle))
