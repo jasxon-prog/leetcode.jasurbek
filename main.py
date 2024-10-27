56d81122216048d56f490c1419fafcd0c6fb7e53
Jasurbek <jasurbekx98@gamil.com>
2024-10-27 15:27:33 +0500

    67-misol

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# # leetcode 58 - misol
+# leetcode 58 - misol
 s = 'Hello world'
 a = s.split()
 b = len(a[-1])
@@ -33,3 +33,9 @@ if a == a[::-1]:
     print(True)
 else: 
     print(False) 
+
+# 67 - misol
+a = '11'
+b = '1'
+summa = int(a, 2) + int(b, 2)
+print(bin(summa)[2:])
