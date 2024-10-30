bb82c361965c10aae99e1627c98b4a2f2c775433
Jasurbek <jasurbekx98@gamil.com>
2024-10-30 17:20:28 +0500

    136-misol

--- main.py
+++ main.py
@@ -69,3 +69,15 @@ def prefix(strs):
         result += min_strs[i]
     return result
 print(prefix(strs))
+
+# 136 - misol
+nums = [2,2,1,1]
+def single_number(nums):
+    num = []
+    for i in nums:
+        if not i in num:
+            num.append(i)
+        else:
+            num.remove(i)
+    return num[0] if num != [] else 0
+print(single_number(nums))
