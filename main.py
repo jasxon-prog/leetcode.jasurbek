5106cbaa002446e61fa83908586d6aa097ce9090
Jasurbek <jasurbekx98@gamil.com>
2024-10-30 19:54:33 +0500

    7-misol

--- main.py
+++ main.py
@@ -81,3 +81,13 @@ def single_number(nums):
             num.remove(i)
     return num[0] if num != [] else 0
 print(single_number(nums))
+
+# 7 - misol
+x = 120
+def reverse_num(x):
+    y = str(abs(x))
+    y = int(y[::-1]) if x > 0 else -int(y[::-1])
+    if not pow(-2, 31) <= y <= pow(2, 31) - 1:
+        return 0
+    return y
+print(reverse_num(x))
