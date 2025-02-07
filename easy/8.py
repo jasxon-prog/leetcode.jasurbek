ef9968340dd5bf7128c8bfba2a780954f534d3c8
jasxon <jasurbekx98@gmail.com>
2025-02-07 15:22:07 +0500

    change

--- easy/8.py
+++ easy/8.py
@@ -2,7 +2,10 @@ def str_int(s: str) -> int:
     s = s.strip()
 
     sign = 1
-    index = 0 # 1
+    index = 0 
+
+    if not s:
+        return 0
 
     if s[0] == '-':
         sign = -1
@@ -15,6 +18,14 @@ def str_int(s: str) -> int:
     while index < len(s) and s[index].isdigit(): 
         result = result * 10 + int(s[index])                             
         index += 1                              
-    return result * sign  
+    result = result * sign  
+
+    int_min, int_max = -2**31, 2**31 - 1
+    if result < int_min:
+        return int_min
+    if result > int_max:
+        return int_max 
     
-print(str_int(s='-111kjvgc555'))
+    return result
+    
+print(str_int(s=''))
